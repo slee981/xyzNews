79571159c2a34eddf4285a30c2e1d1f0c1d870a4
Stephen Lee <smlee.981@gmail.com>
2019-06-27 08:40:28 -0500

    add more files for testing

--- code/website/app/test/test_article.py
+++ code/website/app/test/test_article.py
@@ -1,15 +1,26 @@
 import requests
+import os
 
 KERAS_REST_API_URL = "http://localhost:5000/predict"
+ARTICLES_DIR = os.path.abspath("articles")
+TEST_DIR = os.getcwd()
 
-ARTICLE = "article_huff.txt"
+# change to articles dir
+# and read in all articles
+os.chdir(ARTICLES_DIR)
+test_articles = [a for a in os.listdir() if ".txt" in a]
 
-with open(ARTICLE, "r") as f:
-    article = f.read()
-payload = {"article": article}
+for article in test_articles:
+    print(f"\n##### Reading {article}")
+    with open(article, "r") as f:
+        article = f.read()
 
-r = requests.post(KERAS_REST_API_URL, data=payload).json()
+    payload = {"article": article}
+    r = requests.post(KERAS_REST_API_URL, data=payload).json()
 
-for source in r["predictions"].keys():
-    res = r["predictions"]
-    print(f"{source}: {round(res[source], 3)}")
+    for source in r["predictions"].keys():
+        res = r["predictions"]
+        print(f"{source}: {round(res[source], 3)}")
+
+# change back to original dir
+os.chdir(TEST_DIR)
